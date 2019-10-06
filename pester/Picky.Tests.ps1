c30c496b87bd36172e0c0f0aca6484910c69b544
Marc-André Moreau <mamoreau@devolutions.net>
2019-10-05 20:40:00 -0400

    picky-rs: initial pester tests

--- pester/Picky.Tests.ps1
+++ pester/Picky.Tests.ps1
@@ -1,17 +1,21 @@
+. "$PSScriptRoot/Private/Base64Url.ps1"
+
 Describe 'Picky tests' {
 	BeforeAll {
 		$picky_url = "http://127.0.0.1:12345"
 		$picky_realm = "contoso.local"
+		$picky_authority = "${picky_realm} Authority"
 		$picky_api_key = "secret"
 		$picky_backend = "mongodb"
 		$picky_database_url = "mongodb://picky-mongo:27017"
 
-		& 'docker' 'stop' 'picky-mongo'
-		& 'docker' 'rm' 'picky-mongo'
-		& 'docker' 'run' '-d' '--network=picky' '--name' 'picky-mongo' `
-			'library/mongo:4.1-bionic'
-
-		Start-Sleep -s 5 # wait for picky-mongo to be ready
+		if ($picky_backend -Eq 'mongodb') {
+			& 'docker' 'stop' 'picky-mongo'
+			& 'docker' 'rm' 'picky-mongo'
+			& 'docker' 'run' '-d' '--network=picky' '--name' 'picky-mongo' `
+				'library/mongo:4.1-bionic'
+			Start-Sleep -s 5 # wait for picky-mongo to be ready
+		}
 
 		& 'docker' 'stop' 'picky-server'
 		& 'docker' 'rm' 'picky-server'
@@ -29,5 +33,63 @@ Describe 'Picky tests' {
 		Write-Host "$picky_url/health"
 		$request = Invoke-WebRequest -Uri $picky_url/health -Method 'GET' -ContentType 'text/plain'
 		$request.StatusCode | Should -Be 200
+	}
+
+	It 'gets the CA chain' {
+		$authority_base64 = ConvertTo-Base64Url $picky_authority
+		$contents = Invoke-RestMethod -Uri $picky_url/chain/$authority_base64 -Method 'GET' `
+			-ContentType 'text/plain'
+
+		$ca_chain = @()
+		# https://stackoverflow.com/questions/45884754/powershell-extract-multiple-occurrences-in-multi-lines
+		$contents | Select-String  -Pattern '(?smi)^-{2,}BEGIN CERTIFICATE-{2,}.*?-{2,}END CERTIFICATE-{2,}' `
+			-Allmatches | ForEach-Object {$_.Matches} | ForEach-Object { $ca_chain += $_.Value }
+
+		$ca_chain.Count | Should -Be 2
+		Set-Content -Value $ca_chain[0] -Path ".\intermediate_ca.pem"
+		Set-Content -Value $ca_chain[1] -Path ".\root_ca.pem"
+
+		$root_ca = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2("root_ca.pem")
+		$intermediate_ca = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2("intermediate_ca.pem")
+
+		$intermediate_ca.Subject | Should -Be "CN=${picky_realm} Authority"
+		$intermediate_ca.Issuer | Should -Be "CN=${picky_realm} Root CA"
+
+		$root_ca.Subject | Should -Be "CN=${picky_realm} Root CA"
+		$root_ca.Issuer | Should -Be "CN=${picky_realm} Root CA"
+	}
+
+	It 'signs certificates' {
+		# https://www.digicert.com/ssl-support/openssl-quick-reference-guide.htm
+		& 'openssl' 'genrsa' '-out' 'test.key' '2048'
+		& 'openssl' 'rsa' '-text' '-in' 'test.key' '-noout'
+		& 'openssl' 'req' '-new' '-key' 'test.key' '-out' 'test.csr' `
+			'-subj' "/CN=test.${picky_realm}"
+		& 'openssl' 'req' '-text' '-in' 'test.csr' '-noout' '-verify'
+
+		$csr = Get-Content ".\test.csr" | Out-String
+
+		$headers = @{
+			"Authorization" = "Bearer $picky_api_key"
+		}
+
+		$payload = [PSCustomObject]@{
+			ca="$picky_authority"
+			csr="$csr"
+		} | ConvertTo-Json
+
+		Write-Host $payload
+
+		$cert = Invoke-RestMethod -Uri $picky_url/signcert -Method 'POST' `
+			-Headers $headers `
+			-ContentType 'application/json' `
+			-Body $payload
+
+		Set-Content -Value $cert -Path ".\test.crt"
+		$leaf_cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2("test.crt")
+
+		Write-Host $leaf_cert
+		$leaf_cert.Subject | Should -Be "CN=test.${picky_realm}"
+		$leaf_cert.Issuer | Should -Be "CN=${picky_realm} Authority"
 	}
 }
---
+++ pester/Private/Base64Url.ps1
@@ -0,0 +1,39 @@
+
+function ConvertTo-Base64Url {
+    [CmdletBinding()]
+    [OutputType('System.String')]
+    param(
+        [Parameter(ParameterSetName='String',Mandatory,Position=0,ValueFromPipeline)]
+        [AllowEmptyString()]
+        [string]$Text,
+        [Parameter(ParameterSetName='String')]
+        [switch]$FromBase64,
+        [Parameter(ParameterSetName='Bytes',Mandatory,Position=0)]
+        [AllowEmptyCollection()]
+        [byte[]]$Bytes
+    )
+
+    Process {
+        if (!$FromBase64) {
+            # get a byte array from the input string
+            if ($PSCmdlet.ParameterSetName -eq 'String') {
+                $Bytes = [Text.Encoding]::UTF8.GetBytes($Text)
+            }
+
+            # standard base64 encoder
+            $s = [Convert]::ToBase64String($Bytes)
+
+        } else {
+            # $Text is already Base64 encoded, we just need the Url'ized version
+            $s = $Text
+        }
+
+        # remove trailing '='s
+        $s = $s.Split('=')[0]
+
+        # 62nd and 63rd char of encoding
+        $s = $s.Replace('+','-').Replace('/','_')
+
+        return $s
+    }
+}
