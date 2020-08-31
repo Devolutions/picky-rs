403c8be8f5bfa1174bb31a0d68b1a945367c333a
Benoît CORTIER <benoit.cortier@fried-world.eu>
2020-08-31 10:04:02 -0400

    picky: RSA parts are unsigned (fix #53)

--- picky/src/key.rs
+++ picky/src/key.rs
@@ -184,20 +184,20 @@ impl PrivateKey {
             .expect("Conversion to BigUint failed!"); // should never happen
 
         Ok(Self(PrivateKeyInfo::new_rsa_encryption(
-            IntegerAsn1::from_signed_bytes_be(modulus.to_bytes_be()),
-            IntegerAsn1::from_signed_bytes_be(public_exponent.to_bytes_be()),
-            IntegerAsn1::from_signed_bytes_be(private_exponent.to_bytes_be()),
+            IntegerAsn1::from_unsigned_bytes_be(modulus.to_bytes_be()),
+            IntegerAsn1::from_unsigned_bytes_be(public_exponent.to_bytes_be()),
+            IntegerAsn1::from_unsigned_bytes_be(private_exponent.to_bytes_be()),
             (
                 // primes
-                IntegerAsn1::from_signed_bytes_be(prime_1.to_bytes_be()),
-                IntegerAsn1::from_signed_bytes_be(prime_2.to_bytes_be()),
+                IntegerAsn1::from_unsigned_bytes_be(prime_1.to_bytes_be()),
+                IntegerAsn1::from_unsigned_bytes_be(prime_2.to_bytes_be()),
             ),
             (
                 // exponents
-                IntegerAsn1::from_signed_bytes_be(exponent_1.to_bytes_be()),
-                IntegerAsn1::from_signed_bytes_be(exponent_2.to_bytes_be()),
+                IntegerAsn1::from_unsigned_bytes_be(exponent_1.to_bytes_be()),
+                IntegerAsn1::from_unsigned_bytes_be(exponent_2.to_bytes_be()),
             ),
-            IntegerAsn1::from_signed_bytes_be(coefficient.to_bytes_be()),
+            IntegerAsn1::from_unsigned_bytes_be(coefficient.to_bytes_be()),
         )))
     }
 
