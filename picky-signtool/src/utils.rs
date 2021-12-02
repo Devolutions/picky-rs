71abb7ef9ec7d3e9affb67e3f39105ff6cbe5098
Alexandr Yusuk <aleksandr.yusuk@apriorit.com>
2021-12-02 17:35:05 +0200

    fix formatting

--- picky-signtool/src/utils.rs
+++ picky-signtool/src/utils.rs
@@ -1,3 +1,4 @@
 pub fn str_to_utf16_bytes(s: &str, buff: &mut Vec<u8>) {
-    s.encode_utf16().for_each(|word| buff.extend_from_slice(&word.to_le_bytes()));
+    s.encode_utf16()
+        .for_each(|word| buff.extend_from_slice(&word.to_le_bytes()));
 }
