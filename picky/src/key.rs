46b96de84789281c42041347148883e23ff8b1a8
shurizzle <me@shurizzle.dev>
2022-06-24 01:30:46 +0200

    Add zeroize to subcrates on feature zeroize

--- picky/src/key.rs
+++ picky/src/key.rs
@@ -80,6 +80,20 @@ impl From<PrivateKey> for PrivateKeyInfo {
 }
 
 impl From<PrivateKey> for SubjectPublicKeyInfo {
+    #[cfg(feature = "zeroize")]
+    fn from(key: PrivateKey) -> Self {
+        match key.0.private_key {
+            PrivateKeyValue::RSA(OctetStringAsn1Container(mut key)) => SubjectPublicKeyInfo::new_rsa_key(
+                std::mem::take(&mut key.modulus),
+                std::mem::take(&mut key.public_exponent),
+            ),
+            PrivateKeyValue::EC(OctetStringAsn1Container(mut key)) => {
+                SubjectPublicKeyInfo::new_ec_key(std::mem::take(&mut key.public_key.0 .0))
+            }
+        }
+    }
+
+    #[cfg(not(feature = "zeroize"))]
     fn from(key: PrivateKey) -> Self {
         match key.0.private_key {
             PrivateKeyValue::RSA(OctetStringAsn1Container(key)) => {
