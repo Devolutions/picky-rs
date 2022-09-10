eee84ad3d2dc7c3733d3110438e1cbcfd57dc555
Alexandr <aleksandr.yusuk@apriorit.com>
2022-09-10 13:35:39 +0300

    picky-krb: pkinit: implement nego request messages and write tests for them

--- picky-krb/src/pkinit.rs
+++ picky-krb/src/pkinit.rs
@@ -1,11 +1,45 @@
 use picky_asn1::wrapper::{
-    Asn1SequenceOf, BitStringAsn1, ExplicitContextTag0, ExplicitContextTag1, ExplicitContextTag2, ExplicitContextTag3,
-    IntegerAsn1, OctetStringAsn1, Optional,
+    Asn1SequenceOf, Asn1SetOf, BMPStringAsn1, BitStringAsn1, ExplicitContextTag0, ExplicitContextTag1,
+    ExplicitContextTag2, ExplicitContextTag3, ImplicitContextTag0, IntegerAsn1, ObjectIdentifierAsn1, OctetStringAsn1,
+    Optional,
 };
 use picky_asn1_x509::{AlgorithmIdentifier, SubjectPublicKeyInfo};
 use serde::{Deserialize, Serialize};
 
-use crate::data_types::KerberosTime;
+use crate::data_types::{KerberosTime, PrincipalName, Realm};
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
+pub struct Pku2uValueInner<T> {
+    identifier: ObjectIdentifierAsn1,
+    value: T,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
+pub struct Pku2uValue<T> {
+    pub inner: Asn1SetOf<Pku2uValueInner<T>>,
+}
+
+// #[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
+// pub struct Pku2uNegoReqMetadataInner {
+//     value: ExplicitContextTag0<OctetStringAsn1>,
+// }
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
+pub struct Pku2uNegoReqMetadata {
+    inner: ImplicitContextTag0<OctetStringAsn1>,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
+pub struct Pku2uNegoBody {
+    pub realm: ExplicitContextTag0<Realm>,
+    pub sname: ExplicitContextTag1<PrincipalName>,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
+pub struct Pku2uNegoReq {
+    pub subject_name: ExplicitContextTag0<Asn1SequenceOf<Pku2uNegoReqMetadata>>,
+    pub metadata: ExplicitContextTag1<Pku2uNegoBody>,
+}
 
 /// [Generation of Client Request](https://www.rfc-editor.org/rfc/rfc4556.html#section-3.2.1)
 /// ```not_rust
@@ -127,4 +161,101 @@ pub struct KdcDhKeyInfo {
     nonce: ExplicitContextTag1<IntegerAsn1>,
     #[serde(default)]
     dh_key_expiration: Optional<Option<ExplicitContextTag2<KerberosTime>>>,
+}
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use oid::ObjectIdentifier;
+    use picky_asn1::restricted_string::{BMPString, IA5String};
+    use picky_asn1::tag::Tag;
+    use picky_asn1::wrapper::{
+        Asn1SequenceOf, Asn1SetOf, BMPStringAsn1, ExplicitContextTag0, ExplicitContextTag1, ImplicitContextTag0,
+        IntegerAsn1, ObjectIdentifierAsn1, OctetStringAsn1,
+    };
+
+    use crate::data_types::{KerberosStringAsn1, PrincipalName};
+
+    use super::{Pku2uNegoBody, Pku2uNegoReq, Pku2uNegoReqMetadata, Pku2uValue, Pku2uValueInner};
+
+    #[test]
+    fn pku2u_nego_req_encode() {
+        let message = Pku2uNegoReq {
+            subject_name: ExplicitContextTag0::from(Asn1SequenceOf::from(vec![
+                Pku2uNegoReqMetadata {
+                    inner: ImplicitContextTag0::from(OctetStringAsn1::from(vec![
+                        48, 77, 49, 75, 48, 73, 6, 3, 85, 4, 3, 30, 66, 0, 77, 0, 83, 0, 45, 0, 79, 0, 114, 0, 103, 0,
+                        97, 0, 110, 0, 105, 0, 122, 0, 97, 0, 116, 0, 105, 0, 111, 0, 110, 0, 45, 0, 80, 0, 50, 0, 80,
+                        0, 45, 0, 65, 0, 99, 0, 99, 0, 101, 0, 115, 0, 115, 0, 32, 0, 91, 0, 50, 0, 48, 0, 50, 0, 49,
+                        0, 93,
+                    ])),
+                },
+                Pku2uNegoReqMetadata {
+                    inner: ImplicitContextTag0::from(OctetStringAsn1::from(vec![
+                        48, 77, 49, 75, 48, 73, 6, 3, 85, 4, 3, 30, 66, 0, 77, 0, 83, 0, 45, 0, 79, 0, 114, 0, 103, 0,
+                        97, 0, 110, 0, 105, 0, 122, 0, 97, 0, 116, 0, 105, 0, 111, 0, 110, 0, 45, 0, 80, 0, 50, 0, 80,
+                        0, 45, 0, 65, 0, 99, 0, 99, 0, 101, 0, 115, 0, 115, 0, 32, 0, 91, 0, 50, 0, 48, 0, 50, 0, 49,
+                        0, 93,
+                    ])),
+                },
+            ])),
+            metadata: ExplicitContextTag1::from(Pku2uNegoBody {
+                realm: ExplicitContextTag0::from(KerberosStringAsn1::from(
+                    IA5String::from_string("WELLKNOWN:PKU2U".into()).unwrap(),
+                )),
+                sname: ExplicitContextTag1::from(PrincipalName {
+                    name_type: ExplicitContextTag0::from(IntegerAsn1::from(vec![2])),
+                    name_string: ExplicitContextTag1::from(Asn1SequenceOf::from(vec![
+                        KerberosStringAsn1::from(IA5String::from_string("TERMSRV".into()).unwrap()),
+                        KerberosStringAsn1::from(IA5String::from_string("AZRDOWN-W10".into()).unwrap()),
+                    ])),
+                }),
+            }),
+        };
+
+        let encoded = picky_asn1_der::to_vec(&message).unwrap();
+
+        assert_eq!(
+            &[
+                48, 129, 171, 160, 129, 168, 48, 129, 165, 48, 81, 128, 79, 48, 77, 49, 75, 48, 73, 6, 3, 85, 4, 3, 30,
+                66, 0, 77, 0, 83, 0, 45, 0, 79, 0, 114, 0, 103, 0, 97, 0, 110, 0, 105, 0, 122, 0, 97, 0, 116, 0, 105,
+                0, 111, 0, 110, 0, 45, 0, 80, 0, 50, 0, 80, 0, 45, 0, 65, 0, 99, 0, 99, 0, 101, 0, 115, 0, 115, 0, 32,
+                0, 91, 0, 50, 0, 48, 0, 50, 0, 49, 0, 93, 48, 39, 128, 37, 48, 35, 49, 33, 48, 31, 6, 3, 85, 4, 3, 19,
+                24, 84, 111, 107, 101, 110, 32, 83, 105, 103, 110, 105, 110, 103, 32, 80, 117, 98, 108, 105, 99, 32,
+                75, 101, 121, 48, 39, 128, 37, 48, 35, 49, 33, 48, 31, 6, 3, 85, 4, 3, 19, 24, 84, 111, 107, 101, 110,
+                32, 83, 105, 103, 110, 105, 110, 103, 32, 80, 117, 98, 108, 105, 99, 32, 75, 101, 121
+            ],
+            encoded.as_slice()
+        );
+    }
+
+    #[test]
+    fn pku2u_value_encode() {
+        let value = Pku2uValue {
+            inner: Asn1SetOf::from(vec![Pku2uValueInner {
+                identifier: ObjectIdentifierAsn1(ObjectIdentifier::try_from("2.5.4.3").unwrap()),
+                value: BMPStringAsn1::from(BMPString::from_str("\0M\0S\0-\0O\0r\0g\0a\0n\0i\0z\0a\0t\0i\0o\0n\0-\0P\02\0P\0-\0A\0c\0c\0e\0s\0s\0 \0[\02\00\02\01\0]").unwrap()),
+            }]),
+        };
+
+        let encoded = picky_asn1_der::to_vec(&value).unwrap();
+
+        assert_eq!(
+            &[
+                48, 77, 49, 75, 48, 73, 6, 3, 85, 4, 3, 30, 66, 0, 77, 0, 83, 0, 45, 0, 79, 0, 114, 0, 103, 0, 97, 0,
+                110, 0, 105, 0, 122, 0, 97, 0, 116, 0, 105, 0, 111, 0, 110, 0, 45, 0, 80, 0, 50, 0, 80, 0, 45, 0, 65,
+                0, 99, 0, 99, 0, 101, 0, 115, 0, 115, 0, 32, 0, 91, 0, 50, 0, 48, 0, 50, 0, 49, 0, 93
+            ],
+            encoded.as_slice()
+        );
+    }
+
+    #[test]
+    fn test_tag() {
+        let tag = Tag::from(0x80);
+
+        println!("{:?}", tag);
+        println!("{:?}", Tag::context_specific_constructed(0));
+    }
 }
