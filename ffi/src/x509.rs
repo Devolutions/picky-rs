e7bfe21326d7e0c0f73bd0d98ea10476e81f256e
Benoît CORTIER <bcortier@proton.me>
2024-01-19 16:32:59 +0900

    feat(ffi): builder for self signed certificates
    Only options for top-level self signed certificates are exposed for now.
    
    Issue: ARC-188

--- ffi/src/x509.rs
+++ ffi/src/x509.rs
@@ -22,15 +22,29 @@ impl From<certificate::CertType> for ffi::CertType {
     }
 }
 
+struct CertificateBuilderInner {
+    valid_from: Option<picky::x509::date::UtcDate>,
+    valid_to: Option<picky::x509::date::UtcDate>,
+    subject_dns_name: Option<String>,
+    issuer_common_name: Option<String>,
+    issuer_key: Option<picky::key::PrivateKey>,
+    self_signed: bool,
+    enable_ku_digital_signature: bool,
+    enable_kp_server_auth: bool,
+}
+
 #[diplomat::bridge]
 pub mod ffi {
-    use crate::date::ffi::UtcDate;
-    use crate::error::ffi::PickyError;
-    use crate::key::ffi::PublicKey;
-    use crate::pem::ffi::Pem;
+    use std::cell::RefCell;
+    use std::fmt::Write;
+
     use diplomat_runtime::DiplomatWriteable;
     use picky::x509::certificate;
-    use std::fmt::Write;
+
+    use crate::date::ffi::UtcDate;
+    use crate::error::ffi::PickyError;
+    use crate::key::ffi::{PrivateKey, PublicKey};
+    use crate::pem::ffi::Pem;
 
     pub enum CertType {
         Root,
@@ -99,6 +113,106 @@ pub mod ffi {
             write!(writeable, "{}", self.0.issuer_name())?;
             writeable.flush();
             Ok(())
+        }
+    }
+
+    #[diplomat::opaque]
+    pub struct CertificateBuilder(RefCell<super::CertificateBuilderInner>);
+
+    impl CertificateBuilder {
+        pub fn new() -> Box<CertificateBuilder> {
+            Box::new(Self(RefCell::new(super::CertificateBuilderInner {
+                valid_from: None,
+                valid_to: None,
+                subject_dns_name: None,
+                issuer_common_name: None,
+                issuer_key: None,
+                self_signed: false,
+                enable_ku_digital_signature: false,
+                enable_kp_server_auth: false,
+            })))
+        }
+
+        pub fn set_valid_from(&self, valid_from: &UtcDate) {
+            self.0.borrow_mut().valid_from = Some(valid_from.0.clone());
+        }
+
+        pub fn set_valid_to(&self, valid_to: &UtcDate) {
+            self.0.borrow_mut().valid_to = Some(valid_to.0.clone());
+        }
+
+        pub fn set_issuer_common_name(&self, name: &str) {
+            self.0.borrow_mut().issuer_common_name = Some(name.to_owned());
+        }
+
+        pub fn set_subject_dns_name(&self, name: &str) {
+            self.0.borrow_mut().subject_dns_name = Some(name.to_owned());
+        }
+
+        pub fn set_issuer_key(&self, key: &PrivateKey) {
+            self.0.borrow_mut().issuer_key = Some(key.0.clone());
+        }
+
+        pub fn set_self_signed(&self, is_self_signed: bool) {
+            self.0.borrow_mut().self_signed = is_self_signed;
+        }
+
+        pub fn set_ku_digital_signature(&self, enable: bool) {
+            self.0.borrow_mut().enable_ku_digital_signature = enable;
+        }
+
+        pub fn set_kp_server_auth(&self, enable: bool) {
+            self.0.borrow_mut().enable_kp_server_auth = enable;
+        }
+
+        pub fn build(&self) -> Result<Box<Cert>, Box<PickyError>> {
+            let mut inner = self.0.borrow_mut();
+
+            if !inner.self_signed {
+                return Err("only self signed certificates are supported by the .NET wrapper".into());
+            }
+
+            let valid_from = inner.valid_from.take().ok_or("valid from date is missing")?;
+            let valid_to = inner.valid_to.take().ok_or("valid to date is missing")?;
+            let issuer_common_name = inner.issuer_common_name.take().ok_or("issuer name is missing")?;
+            let issuer_key = inner.issuer_key.take().ok_or("issuer key is missing")?;
+            let subject_dns_name = inner.subject_dns_name.take().ok_or("subject dns name is missing")?;
+
+            let common_name = picky::x509::name::DirectoryName::new_common_name(issuer_common_name);
+            let dns_name = picky::x509::name::GeneralNames::new_dns_name(
+                picky_asn1::restricted_string::Ia5String::from_string(subject_dns_name)
+                    .map_err(|_| "invalid charset for DNS name (issuer name)")?,
+            );
+
+            let mut key_usage = picky::x509::extension::KeyUsage::default();
+
+            if inner.enable_ku_digital_signature {
+                key_usage.set_digital_signature(true);
+            }
+
+            let mut extended_usages = Vec::new();
+
+            if inner.enable_kp_server_auth {
+                extended_usages.push(picky::oids::kp_server_auth());
+            }
+
+            let extended_key_usage = picky::x509::extension::ExtendedKeyUsage::new(extended_usages);
+
+            let cert = certificate::CertificateBuilder::new()
+                .validity(valid_from, valid_to)
+                .self_signed(common_name, &issuer_key)
+                .signature_hash_type(picky::signature::SignatureAlgorithm::RsaPkcs1v15(
+                    picky::hash::HashAlgorithm::SHA2_512,
+                ))
+                .key_id_gen_method(picky::x509::KeyIdGenMethod::SPKFullDER(
+                    picky::hash::HashAlgorithm::SHA2_512,
+                ))
+                .subject_alt_name(dns_name)
+                .key_usage(key_usage)
+                .extended_key_usage(extended_key_usage)
+                .build()?;
+
+            Ok(Box::new(Cert(cert)))
         }
     }
 }
