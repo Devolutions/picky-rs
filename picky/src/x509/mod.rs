9e9daf72708c7e21160936c8131549880fae34d3
irving ou <jou@devolutions.net>
2024-03-12 21:34:32 -0400

    review fix: undo changing picky exposed mod

--- picky/src/x509/mod.rs
+++ picky/src/x509/mod.rs
@@ -142,7 +142,6 @@ pub mod name;
 pub use certificate::Cert;
 pub use csr::Csr;
 pub use key_id_gen_method::KeyIdGenMethod;
-pub use picky_asn1_x509::AlgorithmIdentifierParameters;
 pub use picky_asn1_x509::{DirectoryString, Extension, Extensions};
 
 pub mod extension {
