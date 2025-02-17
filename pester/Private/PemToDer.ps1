6071d28c626ce95df955275703ff6d909466bff6
Benoît Cortier <3809077+CBenoit@users.noreply.github.com>
2025-02-18 03:18:43 +0900

    chore: nuke picky-server (#347)

--- pester/Private/PemToDer.ps1
+++
@@ -1,6 +0,0 @@
-function PemToDer([String] $pem) {
-    $base64 = $pem -Replace "`n","" -Replace "`r","" `
-                   -Replace "-----BEGIN CERTIFICATE-----", "" `
-                   -Replace "-----END CERTIFICATE-----", ""
-    return [Convert]::FromBase64String($base64)
-}
--- picky-server/src/db/config.rs
+++
@@ -1,6 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct DatabaseConfig {
-    pub schema_version: u8,
-}
